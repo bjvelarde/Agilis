6555074bc7befb21662a1ec80b7fec142ce3c1e8
Benjie Velarde <bvelarde@gmail.com>
2013-10-08 16:46:14 +0800

    migration generator fix

--- framework/migration.php
+++ framework/migration.php
@@ -16,7 +16,7 @@ abstract class Migration {
         $name  = (String::is_singular($name)) ? String::pluralize($name)->to_s : $name;
         $file  = APP_ROOT . self::DIR . date('YmdHis') . '-' . String::camelize($name)->underscore() . '.php';
         $class = String::camelize($name);
-        $contents = "<?php\use Agilis\\Migration;\nuse Agilis\\Table;\nuse Agilis\\Translator;\n\nclass $class extends Migration {\n\n    public function up() {\n    }\n\n    public function down() {\n    }\n}\n?>";        
+        $contents = "<?php\nuse Agilis\\Migration;\nuse Agilis\\Table;\nuse Agilis\\Translator;\n\nclass $class extends Migration {\n\n    public function up() {\n    }\n\n    public function down() {\n    }\n}\n?>";        
         if (file_put_contents($file, $contents)) {
             if (substr($name, 0, 7) == 'create_') {
                 $model = String::substr($name, 7)->singularize()->to_s;
